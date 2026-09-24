f117ce0901ac8f32666ed5075afa132c922c0863
FF0Y <miroslav426069@gmail.com>
2026-09-25 00:55:34 +0400

    Добавлены комментарии в файлы circle.py и square.py для лучшего понимания, чтот выполняют программы

--- circle.py
+++ circle.py
@@ -2,8 +2,10 @@ import math
 
 
 def area(r):
+    '''Принимает число r и возвращает площадь круга с радиусом r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r и возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
